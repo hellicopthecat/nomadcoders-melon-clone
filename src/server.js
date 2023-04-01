a14367a1d7ccea500706fef8ec7d0928a3d72d7c
hellicopthecat <114468016+hellicopthecat@users.noreply.github.com>
2023-04-02 00:11:02 +0900

    BoilerPlate Set Up

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 import session from "express-session";
 import flash from "express-flash";
 import MongoStore from "connect-mongo";
-import { localsMiddleware } from "./middlewares";
+import {localsMiddleware} from "./middlewares";
 import rootRouter from "./routers/rootRouter";
 
 const app = express();
@@ -12,16 +12,17 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(
   session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    store: MongoStore.create({mongoUrl: process.env.DB_URL}),
   })
 );
+
 app.use(flash());
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
